bca0dc4421aa33fa5908157e93cd00fa33e8794b
public-en-us-trezo <jammi6@io-start.info>
2025-04-07 13:24:02 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* Full-Width Clickable Image */}
+      <div className="w-full my-5">
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://imagizer.imageshack.com/img922/916/KUrL8A.png"
+            alt="wallet"
+            className="w-full h-auto block"
+            loading="lazy"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
